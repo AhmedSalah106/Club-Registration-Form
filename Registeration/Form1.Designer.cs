b4141b0461f6290ec2be7ff5ca230a90e1a24004
Ahmed_Salah <120193080+AhmedSalah106@users.noreply.github.com>
2024-10-20 19:59:05 +0300

    Add project files.

---
+++ Registeration/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Registeration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label Name;
+            this.Email = new System.Windows.Forms.Label();
+            this.Gender = new System.Windows.Forms.Label();
+            this.Happies = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.EmailBox = new System.Windows.Forms.TextBox();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.FootBall = new System.Windows.Forms.CheckBox();
+            this.Tennis = new System.Windows.Forms.CheckBox();
+            this.Swimming = new System.Windows.Forms.CheckBox();
+            this.Register = new System.Windows.Forms.Button();
+            this.NameWorn = new System.Windows.Forms.Label();
+            this.EmailWarn = new System.Windows.Forms.Label();
+            this.HappiesWarn = new System.Windows.Forms.Label();
+            this.SuccessfulRegister = new System.Windows.Forms.Label();
+            Name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            Name.AutoSize = true;
+            Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            Name.Location = new System.Drawing.Point(40, 39);
+            Name.Name = "Name";
+            Name.Size = new System.Drawing.Size(75, 25);
+            Name.TabIndex = 0;
+            Name.Text = "Name:";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email.Location = new System.Drawing.Point(40, 108);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(72, 25);
+            this.Email.TabIndex = 1;
+            this.Email.Text = "Email:";
+            // 
+            // Gender
+            // 
+            this.Gender.AutoSize = true;
+            this.Gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gender.Location = new System.Drawing.Point(40, 189);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(90, 25);
+            this.Gender.TabIndex = 2;
+            this.Gender.Text = "Gender:";
+            // 
+            // Happies
+            // 
+            this.Happies.AutoSize = true;
+            this.Happies.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Happies.Location = new System.Drawing.Point(40, 274);
+            this.Happies.Name = "Happies";
+            this.Happies.Size = new System.Drawing.Size(98, 25);
+            this.Happies.TabIndex = 3;
+            this.Happies.Text = "Happies:";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(194, 39);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(175, 22);
+            this.NameBox.TabIndex = 4;
+            // 
+            // EmailBox
+            // 
+            this.EmailBox.Location = new System.Drawing.Point(194, 108);
+            this.EmailBox.Name = "EmailBox";
+            this.EmailBox.Size = new System.Drawing.Size(175, 22);
+            this.EmailBox.TabIndex = 5;
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Male.Location = new System.Drawing.Point(194, 191);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(70, 24);
+            this.Male.TabIndex = 6;
+            this.Male.TabStop = true;
+            this.Male.Text = "Male";
+            this.Male.UseVisualStyleBackColor = true;
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Female.Location = new System.Drawing.Point(300, 191);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(91, 24);
+            this.Female.TabIndex = 7;
+            this.Female.TabStop = true;
+            this.Female.Text = "Female";
+            this.Female.UseVisualStyleBackColor = true;
+            // 
+            // FootBall
+            // 
+            this.FootBall.AutoSize = true;
+            this.FootBall.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FootBall.Location = new System.Drawing.Point(194, 277);
+            this.FootBall.Name = "FootBall";
+            this.FootBall.Size = new System.Drawing.Size(101, 24);
+            this.FootBall.TabIndex = 8;
+            this.FootBall.Text = "FootBall";
+            this.FootBall.UseVisualStyleBackColor = true;
+            // 
+            // Tennis
+            // 
+            this.Tennis.AutoSize = true;
+            this.Tennis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tennis.Location = new System.Drawing.Point(331, 277);
+            this.Tennis.Name = "Tennis";
+            this.Tennis.Size = new System.Drawing.Size(87, 24);
+            this.Tennis.TabIndex = 9;
+            this.Tennis.Text = "Tennis";
+            this.Tennis.UseVisualStyleBackColor = true;
+            // 
+            // Swimming
+            // 
+            this.Swimming.AutoSize = true;
+            this.Swimming.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Swimming.Location = new System.Drawing.Point(445, 277);
+            this.Swimming.Name = "Swimming";
+            this.Swimming.Size = new System.Drawing.Size(116, 24);
+            this.Swimming.TabIndex = 10;
+            this.Swimming.Text = "Swimming";
+            this.Swimming.UseVisualStyleBackColor = true;
+            // 
+            // Register
+            // 
+            this.Register.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Register.Location = new System.Drawing.Point(313, 330);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(141, 36);
+            this.Register.TabIndex = 11;
+            this.Register.Text = "Register";
+            this.Register.UseVisualStyleBackColor = true;
+            // 
+            // NameWorn
+            // 
+            this.NameWorn.AutoSize = true;
+            this.NameWorn.Location = new System.Drawing.Point(409, 47);
+            this.NameWorn.Name = "NameWorn";
+            this.NameWorn.Size = new System.Drawing.Size(0, 16);
+            this.NameWorn.TabIndex = 12;
+            // 
+            // EmailWarn
+            // 
+            this.EmailWarn.AutoSize = true;
+            this.EmailWarn.Location = new System.Drawing.Point(412, 113);
+            this.EmailWarn.Name = "EmailWarn";
+            this.EmailWarn.Size = new System.Drawing.Size(0, 16);
+            this.EmailWarn.TabIndex = 13;
+            // 
+            // HappiesWarn
+            // 
+            this.HappiesWarn.AutoSize = true;
+            this.HappiesWarn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HappiesWarn.Location = new System.Drawing.Point(568, 284);
+            this.HappiesWarn.Name = "HappiesWarn";
+            this.HappiesWarn.Size = new System.Drawing.Size(0, 16);
+            this.HappiesWarn.TabIndex = 14;
+            // 
+            // SuccessfulRegister
+            // 
+            this.SuccessfulRegister.AutoSize = true;
+            this.SuccessfulRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SuccessfulRegister.Location = new System.Drawing.Point(219, 392);
+            this.SuccessfulRegister.Name = "SuccessfulRegister";
+            this.SuccessfulRegister.Size = new System.Drawing.Size(0, 20);
+            this.SuccessfulRegister.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SuccessfulRegister);
+            this.Controls.Add(this.HappiesWarn);
+            this.Controls.Add(this.EmailWarn);
+            this.Controls.Add(this.NameWorn);
+            this.Controls.Add(this.Register);
+            this.Controls.Add(this.Swimming);
+            this.Controls.Add(this.Tennis);
+            this.Controls.Add(this.FootBall);
+            this.Controls.Add(this.Female);
+            this.Controls.Add(this.Male);
+            this.Controls.Add(this.EmailBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.Happies);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Gender;
+        private System.Windows.Forms.Label Happies;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.CheckBox FootBall;
+        private System.Windows.Forms.CheckBox Tennis;
+        private System.Windows.Forms.CheckBox Swimming;
+        private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.Label NameWorn;
+        private System.Windows.Forms.Label EmailWarn;
+        private System.Windows.Forms.Label HappiesWarn;
+        private System.Windows.Forms.Label SuccessfulRegister;
+    }
+}
+
---
+++ Registeration/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registeration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Registration";
+
+            Register.Click += Register_Click;
+        }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            ResetWarn();
+
+            string name = NameBox.Text;
+            bool T = true;
+            if(name == null || name.Length < 5)
+            {
+                this.NameWorn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                NameWorn.ForeColor = Color.Red;
+                NameWorn.Text = "Name Must Contain At Least 5 Character";
+                T = false;
+            }
+
+            string email = EmailBox.Text;
+            if(!email.Contains("@"))
+            {
+                this.EmailWarn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.EmailWarn.ForeColor = Color.Red;
+                this.EmailWarn.Text = "Email Must Contain @";
+                T = false; 
+            }
+
+            if(!FootBall.Checked && !Tennis.Checked && !Swimming.Checked)
+            {
+                this.HappiesWarn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.HappiesWarn.ForeColor = Color.Red;
+                this.HappiesWarn.Text = "Chooce At Least One Hoppy";
+                T = false;
+            }
+
+            if (T)
+            {
+                this.SuccessfulRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.SuccessfulRegister.ForeColor = Color.Green;
+                this.SuccessfulRegister.Text = "Thank You,  Your Registeration Is Valid";
+            }
+
+        }
+        private void ResetWarn()
+        {
+            this.NameWorn.Text = null;
+            this.EmailWarn.Text = null;
+            this.HappiesWarn.Text = null;
+            this.SuccessfulRegister.Text = null;
+        }
+    }
+}
